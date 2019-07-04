7e4c1314e4d4d416532a006dbbdbd85c2d579a2d
Mikuhann <ng.kano@gmail.com>
2019-07-04 15:55:21 +0300

    add spinner and styling add github secret and client id

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
+import axios from "axios";
 
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users"
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  componentDidMount() {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({
+      loading: true
+    });
 
+    const res = await axios.get(
+      `https://api.github.com/users?${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+                                        ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({
+      users: res.data,
+      loading: false
+    });
   }
 
   render() {
+    const {users, loading} = this.state;
     return (
       <div className="App">
         <Navbar title="Github Search" icon="fab fa-github"/>
         <div className="container">
-          <Users />
+          <Users users={users} loading={loading} />
         </div>
       </div>
     );
