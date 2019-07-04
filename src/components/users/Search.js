f0c2cd773b50d5d3ee5eb4c697fd92fb38d45ee6
Mikuhann <ng.kano@gmail.com>
2019-07-04 17:55:01 +0300

    add proptypes for search component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
+import PropTypes from "prop-types";
 
 class Search extends Component {
   state = {
     text: ''
   };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
   onChange = (e) => {
     this.setState({[e.target.name]: e.target.value});
   };
+
   onSubmit = (e) => {
     e.preventDefault();
     this.props.searchUsers(this.state.text);
@@ -14,6 +21,7 @@ class Search extends Component {
       text: ''
     });
   };
+
   render() {
     const {text} = this.state;
     return (
