66fdecdb96802979ecf70cfe13630248857ab9a5
Mikuhann <ng.kano@gmail.com>
2019-07-08 17:49:26 +0300

    alert timeout fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, Fragment} from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import axios from "axios";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
@@ -58,16 +58,21 @@ const App = () => {
     setLoading(false);
   };
 
-  let alertTimeout;
+
 
   const showAlert = (msg, type) => {
     setAlert({msg, type});
-    alertTimeout = setTimeout(() => setAlert(null),3000);
   };
+
+  useEffect(() => {
+    let alertTimeout = setInterval(() => setAlert(null), 3000);
+    return () => {
+      clearTimeout(alertTimeout);
+    };
+  }, []);
 
   const closeAlert = () => {
     setAlert(null);
-    clearTimeout(alertTimeout)
   };
   return (
     <Router>
