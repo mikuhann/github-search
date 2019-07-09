51db419c965f8ba76e48ca721a1ea3f4707b328c
Mikuhann <ng.kano@gmail.com>
2019-07-09 14:30:50 +0300

    add home and not found components

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="all-center">
+      <h1><span className="text-danger">Not</span> Found</h1>
+      <p className="lead">The page does not exist</p>
+    </div>
+  );
+};
+
+export default NotFound;
